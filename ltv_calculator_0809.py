2c5bb5583595096403a71a4455185b20e0943131
Kenneth <ding1993yu08@outlook.com>
2023-08-09 14:26:30 +0800

    Update ltv_calculator_0809.py

--- ltv_calculator_0809.py
+++ ltv_calculator_0809.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 
